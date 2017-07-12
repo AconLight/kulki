a91ae146fa6ded3e4fc30202c852619de7701c17
komp\Wojtek <w.mosienski@gmail.com>
2017-07-13 00:37:07 +0200

    initial commit - libgdx framework

---
+++ html/src/com/redartedgames/kulki/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.redartedgames.kulki.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.redartedgames.kulki.KulkiGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new KulkiGame();
+        }
+}
